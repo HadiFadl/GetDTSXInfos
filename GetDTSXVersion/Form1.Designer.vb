d6d2f9d6b675b4778634b0fe0bd5f954c9ff05b0
Admin <Admin@VAIO>
2017-05-12 14:56:08 +0300

    Add project files.

---
+++ GetDTSXVersion/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.rdRegEx = New System.Windows.Forms.RadioButton()
+        Me.rdXmlParser = New System.Windows.Forms.RadioButton()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(732, 8)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(35, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 11)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(714, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 75)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(779, 475)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(692, 45)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(75, 23)
+        Me.btnStart.TabIndex = 3
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.rdXmlParser)
+        Me.Panel1.Controls.Add(Me.rdRegEx)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.btnStart)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(779, 75)
+        Me.Panel1.TabIndex = 4
+        '
+        'rdRegEx
+        '
+        Me.rdRegEx.AutoSize = True
+        Me.rdRegEx.Checked = True
+        Me.rdRegEx.Location = New System.Drawing.Point(12, 45)
+        Me.rdRegEx.Name = "rdRegEx"
+        Me.rdRegEx.Size = New System.Drawing.Size(56, 17)
+        Me.rdRegEx.TabIndex = 4
+        Me.rdRegEx.TabStop = True
+        Me.rdRegEx.Text = "RegEx"
+        Me.rdRegEx.UseVisualStyleBackColor = True
+        '
+        'rdXmlParser
+        '
+        Me.rdXmlParser.AutoSize = True
+        Me.rdXmlParser.Location = New System.Drawing.Point(108, 45)
+        Me.rdXmlParser.Name = "rdXmlParser"
+        Me.rdXmlParser.Size = New System.Drawing.Size(75, 17)
+        Me.rdXmlParser.TabIndex = 5
+        Me.rdXmlParser.Text = "Xml Parser"
+        Me.rdXmlParser.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(779, 550)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnStart As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents rdXmlParser As RadioButton
+    Friend WithEvents rdRegEx As RadioButton
+End Class
---
+++ GetDTSXVersion/Form1.vb
@@ -0,0 +1,193 @@
+﻿Imports System.Text.RegularExpressions
+Imports System.IO
+Imports System.Xml
+
+Public Class Form1
+
+    Private m_strFolder As String = String.Empty
+    Private m_lst As New List(Of PackageInfo)
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Using fbd As New FolderBrowserDialog
+
+            fbd.ShowDialog()
+
+            m_strFolder = fbd.SelectedPath
+
+            TextBox1.Text = m_strFolder
+
+        End Using
+
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        If Not String.IsNullOrEmpty(m_strFolder) Then
+
+            If rdRegEx.Checked Then
+                ReadPackagesInfo(m_strFolder)
+            Else
+                ReadPackagesInfoUsingXmlParser(m_strFolder)
+            End If
+
+
+            DataGridView1.DataSource = m_lst
+            DataGridView1.Refresh()
+        Else
+            MsgBox("You have to select a Source Folder")
+
+        End If
+
+
+    End Sub
+
+    Private Sub ReadPackagesInfo(ByVal strDirectory As String)
+
+
+        m_lst.Clear()
+
+        For Each strFile As String In IO.Directory.GetFiles(strDirectory, "*.dtsx", IO.SearchOption.AllDirectories)
+
+
+            Dim strContent As String = ""
+
+            Using sr As New IO.StreamReader(strFile)
+
+                strContent = sr.ReadToEnd
+                sr.Close()
+
+            End Using
+
+
+            Dim strPackageFormatVersion As String = Regex.Match(strContent, "(?<=""PackageFormatVersion"">)(.*)(?=</DTS:Property>)", RegexOptions.Singleline).Value
+            Dim strCreationDate As String = Regex.Match(strContent, "(?<=DTS:CreationDate="")(.*?)(?="")", RegexOptions.Singleline).Value
+            Dim strCreationName As String = Regex.Match(strContent, "(?<=DTS:CreationName="")(.*?)(?="")", RegexOptions.Singleline).Value
+            Dim strCreatorComputerName As String = Regex.Match(strContent, "(?<=DTS:CreatorComputerName="")(.*?)(?="")", RegexOptions.Singleline).Value
+            Dim strCreatorName As String = Regex.Match(strContent, "(?<=DTS:CreatorName="")(.*?)(?="")", RegexOptions.Singleline).Value
+            Dim strDTSID As String = Regex.Match(strContent, "(?<=DTS:DTSID="")(.*?)(?="")", RegexOptions.Singleline).Value
+            Dim strExecutableType As String = Regex.Match(strContent, "(?<=DTS:ExecutableType="")(.*?)(?="")", RegexOptions.Singleline).Value
+            Dim strLastModifiedProductVersion As String = Regex.Match(strContent, "(?<=DTS:LastModifiedProductVersion="")(.*?)(?="")", RegexOptions.Singleline).Value
+            Dim strLocaleID As String = Regex.Match(strContent, "(?<=DTS:LocaleID="")(.*?)(?="")", RegexOptions.Singleline).Value
+            Dim strObjectName As String = Regex.Match(strContent, "(?<=DTS:ObjectName="")(.*?)(?="")", RegexOptions.Singleline).Value
+            Dim strPackageType As String = Regex.Match(strContent, "(?<=DTS:PackageType="")(.*?)(?="")", RegexOptions.Singleline).Value
+            Dim strVersionBuild As String = Regex.Match(strContent, "(?<=DTS:VersionBuild="")(.*?)(?="")", RegexOptions.Singleline).Value
+            Dim strVersionGUID As String = Regex.Match(strContent, "(?<=DTS:VersionGUID="")(.*?)(?="")", RegexOptions.Singleline).Value
+
+
+
+            m_lst.Add(New PackageInfo With {.PackageFileName = strFile,
+                          .PackageFormatVersion = strPackageFormatVersion,
+                          .CreationDate = strCreationDate,
+                          .CreationName = strCreationName,
+                          .CreatorComputerName = strCreatorComputerName,
+                          .CreatorName = strCreatorName,
+                          .DTSID = strDTSID,
+                          .ExecutableType = strExecutableType,
+                          .LastModifiedProductVersion = strLastModifiedProductVersion,
+                          .LocaleID = strLocaleID,
+                          .ObjectName = strObjectName,
+                          .PackageType = strPackageType,
+                          .VersionBuild = strVersionBuild,
+                         .VersionGUID = strVersionGUID})
+
+
+        Next
+
+
+
+    End Sub
+
+    Private Sub ReadPackagesInfoUsingXmlParser(ByVal strDirectory As String)
+
+        m_lst.Clear()
+
+        For Each strFile As String In IO.Directory.GetFiles(strDirectory, "*.dtsx", IO.SearchOption.AllDirectories)
+
+            Dim strPackageFormatVersion As String = ""
+            Dim strCreationDate As String = ""
+            Dim strCreationName As String = ""
+            Dim strCreatorComputerName As String = ""
+            Dim strCreatorName As String = ""
+            Dim strDTSID As String = ""
+            Dim strExecutableType As String = ""
+            Dim strLastModifiedProductVersion As String = ""
+            Dim strLocaleID As String = ""
+            Dim strObjectName As String = ""
+            Dim strPackageType As String = ""
+            Dim strVersionBuild As String = ""
+            Dim strVersionGUID As String = ""
+
+
+            Dim xml = XDocument.Load(strFile)
+
+            Dim ns As XNamespace = "www.microsoft.com/SqlServer/Dts"
+            Dim man As XmlNamespaceManager = New XmlNamespaceManager(New NameTable())
+            man.AddNamespace("DTS", "www.microsoft.com/SqlServer/Dts")
+
+            If Not xml.Root Is Nothing AndAlso
+                    Not xml.Root.Descendants(ns + "Property").Attributes(ns + "Name") Is Nothing AndAlso
+                         xml.Root.Descendants(ns + "Property").Attributes(ns + "Name").Where(Function(x) x.Value = "PackageFormatVersion").Count > 0 Then
+
+                strPackageFormatVersion = xml.Root.Descendants(ns + "Property").Attributes(ns + "Name").Where(Function(x) x.Value = "PackageFormatVersion").FirstOrDefault.Parent.Value
+
+                strCreationDate = If(xml.Root.Attributes(ns + "CreationDate").FirstOrDefault Is Nothing, "", xml.Root.Attributes(ns + "CreationDate").FirstOrDefault.Value)
+                strCreationName = If(xml.Root.Attributes(ns + "CreationName").FirstOrDefault Is Nothing, "", xml.Root.Attributes(ns + "CreationName").FirstOrDefault.Value)
+                strCreatorComputerName = If(xml.Root.Attributes(ns + "CreatorComputerName").FirstOrDefault Is Nothing, "", xml.Root.Attributes(ns + "CreatorComputerName").FirstOrDefault.Value)
+                strCreatorName = If(xml.Root.Attributes(ns + "CreatorName").FirstOrDefault Is Nothing, "", xml.Root.Attributes(ns + "CreatorName").FirstOrDefault.Value)
+                strDTSID = If(xml.Root.Attributes(ns + "DTSID").FirstOrDefault Is Nothing, "", xml.Root.Attributes(ns + "DTSID").FirstOrDefault.Value)
+                strExecutableType = If(xml.Root.Attributes(ns + "ExecutableType").FirstOrDefault Is Nothing, "", xml.Root.Attributes(ns + "ExecutableType").FirstOrDefault.Value)
+                strLastModifiedProductVersion = If(xml.Root.Attributes(ns + "LastModifiedProductVersion").FirstOrDefault Is Nothing, "", xml.Root.Attributes(ns + "LastModifiedProductVersion").FirstOrDefault.Value)
+                strLocaleID = If(xml.Root.Attributes(ns + "LocaleID").FirstOrDefault Is Nothing, "", xml.Root.Attributes(ns + "LocaleID").FirstOrDefault.Value)
+                strObjectName = If(xml.Root.Attributes(ns + "ObjectName").FirstOrDefault Is Nothing, "", xml.Root.Attributes(ns + "ObjectName").FirstOrDefault.Value)
+                strPackageType = If(xml.Root.Attributes(ns + "PackageType").FirstOrDefault Is Nothing, "", xml.Root.Attributes(ns + "PackageType").FirstOrDefault.Value)
+                strVersionBuild = If(xml.Root.Attributes(ns + "VersionBuild").FirstOrDefault Is Nothing, "", xml.Root.Attributes(ns + "VersionBuild").FirstOrDefault.Value)
+                strVersionGUID = If(xml.Root.Attributes(ns + "VersionGUID").FirstOrDefault Is Nothing, "", xml.Root.Attributes(ns + "VersionGUID").FirstOrDefault.Value)
+            End If
+
+
+
+            m_lst.Add(New PackageInfo With {.PackageFileName = strFile,
+                          .PackageFormatVersion = strPackageFormatVersion,
+                          .CreationDate = strCreationDate,
+                          .CreationName = strCreationName,
+                          .CreatorComputerName = strCreatorComputerName,
+                          .CreatorName = strCreatorName,
+                          .DTSID = strDTSID,
+                          .ExecutableType = strExecutableType,
+                          .LastModifiedProductVersion = strLastModifiedProductVersion,
+                          .LocaleID = strLocaleID,
+                          .ObjectName = strObjectName,
+                          .PackageType = strPackageType,
+                          .VersionBuild = strVersionBuild,
+                         .VersionGUID = strVersionGUID})
+
+        Next
+
+    End Sub
+
+
+
+
+
+End Class
+
+Public Class PackageInfo
+
+    Public Property PackageFileName As String
+    Public Property PackageFormatVersion As String
+    Public Property CreationDate As String
+    Public Property CreationName As String
+    Public Property CreatorComputerName As String
+    Public Property CreatorName As String
+    Public Property DTSID As String
+    Public Property ExecutableType As String
+    Public Property LastModifiedProductVersion As String
+    Public Property LocaleID As String
+    Public Property ObjectName As String
+    Public Property PackageType As String
+    Public Property VersionBuild As String
+    Public Property VersionGUID As String
+
+
+End Class
